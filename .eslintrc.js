273a5fcd6c5f6c8ae2c02deb2bb62503e422af81
Antonio Gil <antonioluisgil@gmail.com>
2017-08-12 14:58:01 +0200

    Added and fixed eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,20 +8,29 @@ module.exports = {
   },
   env: {
     browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: 'eslint:recommended',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['warn'],
+    'no-console': 0,
   }
 }
