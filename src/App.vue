40c6efa58365491d462cc9399abdb1d7eb349e85
Antonio Gil <antonioluisgil@gmail.com>
2017-10-27 12:16:59 +0200

    Updated Vuetify version

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,24 @@
   <v-app id="app">
     <app-sidebar></app-sidebar>
     <app-header></app-header>
-    <main>
-      <v-content>
-        <v-container fluid fill-height v-show="$store.state.loading">
-          <v-layout align-center justify-center>
-            <v-progress-circular
-              indeterminate
-              :size="75"
-              :color="$store.getters.primaryColor">
-            </v-progress-circular>
-          </v-layout>
-        </v-container>
-        <v-container fluid v-show="!$store.state.loading">
-          <v-layout row>
-            <v-flex xs12 sm8 offset-sm2>
-              <router-view></router-view>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-    </main>
+    <v-content>
+      <v-container fluid fill-height v-show="$store.state.loading">
+        <v-layout align-center justify-center>
+          <v-progress-circular
+            indeterminate
+            :size="75"
+            :color="$store.getters.primaryColor">
+          </v-progress-circular>
+        </v-layout>
+      </v-container>
+      <v-container fluid v-show="!$store.state.loading">
+        <v-layout row>
+          <v-flex xs12 sm8 offset-sm2>
+            <router-view></router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
